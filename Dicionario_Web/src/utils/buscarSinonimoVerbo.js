bfff6a604501443a4a9eb88f3e9a5f7e035032e9
João Victor <j.victorbahia@gmail.com>
2023-09-09 21:19:28 -0300

    funcionalidade para buscar os verbos e sinonimos da palavra

---
+++ Dicionario_Web/src/utils/buscarSinonimoVerbo.js
@@ -0,0 +1,17 @@
+export default function buscarSinonimosVerbos(array , tipo) {
+    const arrayValores = []
+    const valores = array.find(item => item.partOfSpeech === tipo)
+
+    if (!valores) {
+        return arrayValores
+    }
+
+    valores.definitions.forEach((item) => {
+        if (arrayValores.length > 4) {
+            return
+        }
+        arrayValores.push(item.definition)
+    })
+
+    return arrayValores
+}

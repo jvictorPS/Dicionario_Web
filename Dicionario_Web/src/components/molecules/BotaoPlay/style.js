437ea755757d636637ead3cddf2146d47501b4ad
João Victor <j.victorbahia@gmail.com>
2023-09-14 23:26:08 -0300

    estilização botão de play

---
+++ Dicionario_Web/src/components/molecules/BotaoPlay/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const ContainerPlay = styled.div`
+    width: 75px;
+    height: 75px;
+    background-color: rgba(164, 69, 237, 0.25);
+    border-radius: 100%;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    :hover {
+        cursor: pointer;
+    }
+`

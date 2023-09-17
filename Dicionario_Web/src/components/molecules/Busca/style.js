0f60a3a8fa28f1746aa8aef678c1ae59e4ab0103
João Victor <j.victorbahia@gmail.com>
2023-09-17 20:33:29 -0300

    inicio da logica para erro de input vazio

--- Dicionario_Web/src/components/molecules/Busca/style.js
+++ Dicionario_Web/src/components/molecules/Busca/style.js
@@ -8,7 +8,9 @@ export const InputBusca = styled.div`
     justify-content: space-between;
     box-sizing: border-box;
     align-items: center;
+    margin: 45px 0;
     padding: 0 20px;
+    border: ${props => props.vazio ? '1px solid red' : 'none'};
 
     &:hover , &:focus {
         border: 1px solid #A445ED;
@@ -50,5 +52,17 @@ export const ContainerPalavra = styled.h1`
     font-size: 55px;
     color: #2D2D2D;
     font-weight: 700;
+`
 
+export const ContainerPrimeiraSessao = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 40px;
+`
+
+export const ContainerFonetica = styled.p`
+    color: #A445ED;
+    font-size: 24px;
+    font-weight: 400;
 `

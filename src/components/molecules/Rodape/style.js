04faa39bf07cb3e4ca67fe7dac08376233c9e1bc
João Victor <j.victorbahia@gmail.com>
2023-10-16 18:01:08 -0300

    ajuste da altura do rodape no layout mobile

--- src/components/molecules/Rodape/style.js
+++ src/components/molecules/Rodape/style.js
@@ -3,7 +3,11 @@ import styled from "styled-components"
 export const ContainerRodape = styled.footer`
     display: flex;
     border-top: 1px solid #E9E9E9;
-    padding-top: 20px
+    padding-top: 20px;
+
+    @media (max-width: 435px) {
+        height: 100px
+    }
 `
 
 export const Source = styled.div`
